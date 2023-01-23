75db79756478a71c60289631472f41726855de48
jochana <joseph.ronaldo902@gmail.com>
2023-01-23 17:21:14 +0100

    project set-up

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './components/header/header.component';
+import {SharedModule} from "../shared/shared.module";
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    SharedModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MatToolbarModule // for Angular material
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
